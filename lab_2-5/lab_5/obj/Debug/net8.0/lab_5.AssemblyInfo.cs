ec02ddf918efcd457b1e2ef68d02ddf6eac30fad
ILYA <colo.il@yandex.ru>
2025-05-18 22:13:28 +0600

    feat(project): Update lab_5

--- lab_2-5/lab_5/obj/Debug/net8.0/lab_5.AssemblyInfo.cs
+++ lab_2-5/lab_5/obj/Debug/net8.0/lab_5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f9b8777466089e0fb4c18adc67f4502b907048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca40d713db6149322500ce82c4874c0e26dbae54")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab_5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
